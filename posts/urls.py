6c08e43ef1f032cc5850acb1db5d7c2638d6dc26
Peyman Golkar <peymanpic00@gmail.com>
2022-01-25 21:29:43 +0330

    add url and view to show posts by category

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('post/<slug>' , views.PostDetailView.as_view() , name='post_detail')
+    path('post/<slug>' , views.PostDetailView.as_view() , name='post_detail'),
+    path('category/<slug>' , views.CategoryPostList.as_view() , name='posts_by_category')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_list_or_404
+from django.views.generic import ListView, TemplateView
 from django.views.generic.detail import DetailView
 from .models import  Post
 
@@ -13,3 +13,12 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post-details.html'
     context_object_name = 'post'
+
+class CategoryPostList(TemplateView):
+    template_name = 'blog.html'
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = context['slug']
+        context['posts'] = get_list_or_404(Post , category__title__icontains=category_slug)
+        return context
