fc29f0392c71799825d0d26bdc7836bdeb98610b
Peyman Golkar <peymanpic00@gmail.com>
2022-01-25 23:24:49 +0330

    fix links , modify home page

--- codeyad_blog/urls.py
+++ codeyad_blog/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view()),
     path('posts/', include('posts.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
