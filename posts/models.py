a2fd00c4b78fd3bad09a4a3c570d8deea1c78995
Peyman Golkar <peymanpic00@gmail.com>
2022-01-25 15:16:06 +0330

    change post model, add author model

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,12 @@ from ckeditor_uploader.fields import RichTextUploadingField
 from taggit.managers import TaggableManager
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 class Category(models.Model):
     title = models.CharField(max_length=100, unique=True)
@@ -21,7 +26,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(unique=True, allow_unicode=True)
     category = models.ManyToManyField(Category)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='post')
     description = models.CharField(max_length=100)
     body = RichTextUploadingField()
     pub_date = models.DateTimeField(auto_now=False, auto_now_add=True)
