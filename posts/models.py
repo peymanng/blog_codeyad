5959d3b7fa1fc7ea4402acbcb2121c57096dfc9e
Peyman Golkar <peymanpic00@gmail.com>
2022-01-25 14:51:38 +0330

    add Category Model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField()
+    description = models.CharField(max_length=130, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
