10241a4e6dc930f8b48b0f127a9fd2296e709bbc
Peyman Golkar <peymanpic00@gmail.com>
2022-01-25 14:58:38 +0330

    add post model

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+from taggit.managers import TaggableManager
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Category(models.Model):
     title = models.CharField(max_length=100, unique=True)
@@ -10,3 +15,24 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, allow_unicode=True)
+    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    description = models.CharField(max_length=100)
+    body = RichTextUploadingField()
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    image = models.ImageField(upload_to='posts/')
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_categories(self):
+        return ' , '.join([category.title for category in self.category.all()])
+
+    def get_absolute_url(self):
+        return f'/posts/post/{self.slug}'
