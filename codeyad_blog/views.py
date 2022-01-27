36473bb098fd411d0a6e78bacab11404d3b1fbc6
Peyman Golkar <peymanpic00@gmail.com>
2022-01-27 13:09:10 +0330

    fix home view and index.html

--- codeyad_blog/views.py
+++ codeyad_blog/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import TemplateView
+from posts.models import Post
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data()
+        slider_posts = Post.objects.all().order_by('-pub_date')[:6]
+        context['slider_posts'] = slider_posts
+        return context
 
 class AboutUsView(TemplateView):
     template_name = 'about.html'
