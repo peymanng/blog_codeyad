7bf9af0dc73c1a08e2cc4895f5b9b57d4e2340f0
Peyman Golkar <peymanpic00@gmail.com>
2022-01-28 18:10:05 +0330

    changed posts/admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -3,10 +3,16 @@ from .models import  Post , Category , Author , Comment
 from mptt.admin import MPTTModelAdmin
 # Register your models here.
 
+class PostInline(admin.StackedInline):
+    model = Comment
+    extra = 1
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title' , 'get_categories', 'slug' , 'tag_list' , 'pub_date')
     prepopulated_fields = {'slug' : ('title' ,)}
+    list_filter = ('category' , 'pub_date' , 'tags')
+    inlines = [PostInline]
 
     def get_queryset(self, request):
         return super().get_queryset(request).prefetch_related('tags')
